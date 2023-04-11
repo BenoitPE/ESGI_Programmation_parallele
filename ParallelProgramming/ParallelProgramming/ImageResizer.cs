a4f6bc8ee8bd57981883cb78eb11650d8f1dee53
BenoitPE <benoitpegaz@gmail.com>
2023-04-11 17:06:08 +0200

    Comparaison single thread vs multi thread

---
+++ ParallelProgramming/ParallelProgramming/ImageResizer.cs
@@ -0,0 +1,60 @@
+﻿namespace ParallelProgramming
+{
+    public class ImageResizer
+    {
+        private List<string> imageFiles = new List<string>();
+        private string inputDirectory = string.Empty;
+        private string outputDirectory = string.Empty;
+
+        public ImageResizer()
+        {
+            inputDirectory = Path.GetFullPath(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\..\Inputs\"));
+            outputDirectory = Path.GetFullPath(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\..\Outputs\"));
+
+            // Charger les fichiers d'images .jpg présents dans le dossier Inputs/
+            imageFiles = new List<string>(Directory.GetFiles(inputDirectory, "*.jpg", SearchOption.TopDirectoryOnly));
+        }
+
+        /// <summary>
+        /// Compression d'image en séquentielle
+        /// </summary>
+        public void SingleThreadImageResizer()
+        {
+            foreach (var file in imageFiles)
+            {
+                string outputFilePath = outputDirectory + Path.GetFileNameWithoutExtension(file) + "_compressed.jpg";
+
+                Resize(file, outputFilePath);
+            }
+        }
+
+        /// <summary>
+        /// Compression d'image en parallèle
+        /// </summary>
+        public void MultiThreadImageResizer()
+        {
+            Parallel.ForEach(imageFiles, new ParallelOptions { MaxDegreeOfParallelism = 30 }, (file) =>
+            {
+                string outputFilePath = outputDirectory + Path.GetFileNameWithoutExtension(file) + "_compressed.jpg";
+
+                Resize(file, outputFilePath);
+            });
+        }
+
+        /// <summary>
+        /// Méthode pour compresser une image en 100x100
+        /// </summary>
+        private static void Resize(string file, string outputFilePath)
+        {
+            using (var image = Image.Load(file))
+            {
+                image.Mutate(x => x.Resize(new ResizeOptions
+                {
+                    Size = new Size(100, 100),
+                    Mode = ResizeMode.Max
+                }));
+                image.Save(outputFilePath);
+            }
+        }
+    }
+}
---
+++ ParallelProgramming/ParallelProgramming/Program.cs
@@ -0,0 +1,32 @@
+﻿using ParallelProgramming;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ImageResizer imageResizer = new ImageResizer();
+
+        Console.WriteLine("\nDébut de la compression des images en séquentiel (single thread)");
+        Stopwatch swSingleThread = new Stopwatch();
+        swSingleThread.Start();
+        imageResizer.SingleThreadImageResizer();
+        swSingleThread.Stop();
+        Console.WriteLine($"Compression séquentielle terminée en {swSingleThread.ElapsedMilliseconds} ms");
+
+        Console.WriteLine("\nDébut de la compression des images en parallèle (multi thread)");
+        Stopwatch swMultiThread = new Stopwatch();
+        swMultiThread.Start();
+        imageResizer.MultiThreadImageResizer();
+        swMultiThread.Stop();
+        Console.WriteLine($"Compression parallèle terminée en {swMultiThread.ElapsedMilliseconds} ms");
+
+        float percentage = swSingleThread.ElapsedMilliseconds - swMultiThread.ElapsedMilliseconds;
+        percentage = percentage / swSingleThread.ElapsedMilliseconds;
+        percentage = percentage * 100;
+        Console.WriteLine($"\n--> La compression parallèle est plus rapide que la compression séquentielle de {percentage}%");
+
+        Console.ReadLine();
+    }
+
+}
